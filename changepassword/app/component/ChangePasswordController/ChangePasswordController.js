8a46cd36bfa99c4c99281788f99b38a6d46f532b
Intuz <manali.s@virtualveb.com>
2018-02-19 20:01:40 +0530

    First Commit

--- changepassword/app/component/ChangePasswordController/ChangePasswordController.js
+++ changepassword/app/component/ChangePasswordController/ChangePasswordController.js
@@ -1,3 +1,15 @@
+// Copyright (C) 2018 INTUZ. 
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import React, {Component} from 'react';
 import {
     AppRegistry,
@@ -55,22 +67,7 @@ class ChangePasswordController extends Component {
      * Call your webservice for update password
      */
     changePassword() {
-        // this.setState({spinnerVisible: true});
-        // WebClient.postRequest(Settings.URL.CHANGE_PASSWORD, {
-        //     "user_id": Utility.user.id,
-        //     "oldpassword": this.state.currentPassword,
-        //     "newpassword": this.state.newPassword
-        // }, (response, error) => {
-        //     this.setState({spinnerVisible: false});
-        //     if (error == null) {
-        //         this.setState({currentPassword: ""})
-        //         this.setState({newPassword: ""})
-        //         this.setState({confirmPassword: ""})
-        //         Utility.showToast("Password change successfully");
-        //     } else {
-        //         Utility.showToast(error.message);
-        //     }
-        // });
+        
     }
 
     render() {
--- changepassword/app/component/ChangePasswordController/index.js
+++ changepassword/app/component/ChangePasswordController/index.js
@@ -1,33 +1,3 @@
 import ChangePasswordController from "./ChangePasswordController";
 export default ChangePasswordController
 
-
-// import React, { Component } from 'react';
-// import {
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   View,
-//   TouchableOpacity,
-//   Modal,
-// } from 'react-native';
-
-// import ChangePasswordController from './app/component/ChangePasswordController';
-
-// export default class App extends Component {
-  
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-
-//         };
-//     }
-
-//   render() {
-//     return (
-//       <View style={{flex:1, margin: 10, backgroundColor:'yellow' }}>
-//         <ChangePasswordController backgroundColor='red' submitText='Send' placeHolderCurrentPassword='Old Password' placeHolderNewPassword='New Password' placeHolderConfirmPassword='Confirm Password' />
-//       </View>
-//     );
-//   }
-// }
